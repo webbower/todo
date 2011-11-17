943cc1fc92ea5301a321f86b3a7b7462cb9ec799
Matt Bower <matt@webbower.com>
2011-11-16 22:55:43 -0800

    Basic session-based todo app working on Firefox, iPad, and Android. Styling Needed.

---
+++ js/lib.js
@@ -0,0 +1,83 @@
+(function() {
+	"use strict";
+	
+	if(!String.prototype.format) {
+		String.prototype.format = function() {
+			var
+				s = this,
+				i = arguments.length
+			;
+			
+			while(i--)
+				s = s.replace(new RegExp('\\{' + i + '\\}', 'gi'), arguments[i]);
+			
+			return s;
+		}
+	}
+})();
+
+(function($) {
+	"use strict";
+	
+	window.Todo = (window.Todo || {});
+	
+	$.extend(window.Todo, {
+		form: null,
+		list: null,
+		
+		templates: {
+			task: '<li id="todo-{0}"><button type="button">Completed</button> {1}</li>'
+		},
+		
+		init: function() {
+			console.log("TODO INIT");
+			this.form = $('#add-form');
+			this.list = $('#tasks');
+			
+			var
+				self = this,
+				tapEvent = Modernizr.touch ? 'touchstart' : 'click'
+			;
+			
+			this.form
+				.on('submit', function(ev) {
+					ev.preventDefault();
+					console.log("ADDING");  // TODO: Remove for Production
+
+					var
+						field = self.form.find('input[type=text]'),
+						todo = field.val()
+					;
+			
+					if(!!todo) {
+						self.addTask(todo);
+						field.val('');
+					}
+				})
+			;
+			
+			this.list
+				.on(tapEvent, 'li button', function(ev) {
+					console.log("COMPLETING");  // TODO: Remove for Production
+
+					self.completeTask($(this).closest('li'));
+				})
+			;
+		},
+		
+		addTask: function(todo) {
+			if(!!todo)
+				this.list.append(this.templates.task.format((new Date()).getTime(), todo));
+		},
+		
+		completeTask: function(taskEl) {
+			if(!!taskEl) {
+				taskEl = (!!taskEl.jquery ? taskEl : $(taskEl));
+				
+				taskEl.fadeOut(400, function() {
+					$(this).remove();
+				});
+			}
+		}
+	});
+})(jQuery);
